4991b8645fc41650523c189f1c184ce8d0642b0e
Sohee-Elizabeth-park <seobeusohui@gmail.com>
2024-05-25 23:00:54 +0900

    Product Manegement Application

---
+++ src/main/java/kr/co/hanbit/product/management/presentation/ProductController.java
@@ -0,0 +1,23 @@
+package kr.co.hanbit.product.management.presentation;
+
+import kr.co.hanbit.product.management.application.SimpleProductService;
+import kr.co.hanbit.product.management.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProductController {
+    
+    private SimpleProductService simpleProductService;
+    
+    @Autowired
+    ProductController(SimpleProductService simpleProductService) {
+        this.simpleProductService = simpleProductService;
+    }
+    
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        //Product를 생성하고 리스트에 넣는 작업이 필요함.
+        return simpleProductService.add(productDto);
+    }
+}
